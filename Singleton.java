840a7e85bc7d102af1ebe4bdd3aae586e1b816b1
Alioune MANE <aliounemane2@gmail.com>
2021-12-12 12:32:28 +0000

    Update Singleton.java

--- Singleton.java
+++ Singleton.java
@@ -1,8 +1,4 @@
 
-// Le pattern singleton est constitué d’une seule classe : 
-// 1)  Convention qui garantit qu’un seul objet est instancié pour une classe donnée Fournit 
-// 2) une méthode de classe unique retournant cette instance
-
 public class Singleton{
     
     private static Singleton uniqueInstance;
