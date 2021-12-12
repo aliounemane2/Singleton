cf86f6a0b5c4a915b978924bf33b33175c09eb23
Alioune MANE <aliounemane2@gmail.com>
2021-12-12 12:42:41 +0000

    Update Singleton.java

--- Singleton.java
+++ Singleton.java
@@ -8,15 +8,14 @@ public class Singleton{
     
     // fournit un moyen d'instancier et d'en trouver une seule instance
     public static Singleton getInstance(){
-        if(uniqueInstance == null)
+        if (uniqueInstance == null)
             uniqueInstance = new Singleton();
-        
         return uniqueInstance; 
     }
     
     public static void main(String []args){
         Singleton s1 = Singleton.getInstance();
-		Singleton s2 = Singleton.getInstance();
-		System.out.println(s1 == s2);
+	Singleton s2 = Singleton.getInstance();
+	System.out.println(s1 == s2);
     }
 }
